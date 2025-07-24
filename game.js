65e23dcb5ca6b5f6bb608e0f1d01faceb090c0d2
Smartstak <smartstak@gmail.com>
2025-07-24 22:49:49 +0530

    updating files

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+  const grid = document.querySelector('.grid');
+  const doodler = document.createElement('div');
+  let doodlerLeftSpace = 50;
+  let doodlerBottomSpace = 150;
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add('doodler');
+    doodler.style.left = `${doodlerLeftSpace}px`;
+    doodler.style.bottom = `${doodlerBottomSpace}px`;
+  }
+  createDoodler();
+
 })
